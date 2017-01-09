21bb40860329e680feede4783ccf4aa2db2646e7
NBOcelot <zzzpppa@gmail.com>
2017-01-08 19:42:25 -0500

    Sunday's Work

--- complexwires/complex.js
+++ complexwires/complex.js
@@ -1,6 +1,7 @@
+
 //declare default bomb parameters
 var serialBool = 0, parallelBool = 0, batteriesBool = 0; //could omit values
-
+/*
 //input method 1 entire serial number or last digit - if even, set true;
 //input method 2 "yes/no" --needs input function
 if (charAt(serial.length) % === 0) {
@@ -14,53 +15,114 @@ if (input(hasParallel) { //placeholder FIX THIS --needs input function
 var batteries = input(batteries); //placeholder FIX THIS --needs input function
 if (batteries > 1) {batteriesBool = 1);
 }
-
+*/
 //--------------------------------------
 
 //put input into array --How do you pull from file?
-var complexWire = [0, 0, 0, 0]; // light, star, red, blue,
-
+var complexWire = [0, 1, 1, 0]; // light, star, red, blue,
 //debug to check input
 console.log("Light: "+complexWire[0]);
 console.log("Star: "+complexWire[1]);
 console.log("Red: "+complexWire[2]);
 console.log("Blue: "+complexWire[3]);
 
-//find solve code --DONE (there's probably a cleaner way to do this)
-var solve;
-if (complexWire === [0, 0, 0, 0]) {solve = C;}
-if (complexWire === [0, 0, 0, 1]) {solve = S;}
-if (complexWire === [0, 0, 1, 1]) {solve = S;}
-if (complexWire === [0, 1, 1, 1]) {solve = P;}
-if (complexWire === [1, 1, 1, 1]) {solve = D;}
-if (complexWire === [0, 1, 0, 1]) {solve = D;}
-if (complexWire === [1, 1, 0, 1]) {solve = P;}
-if (complexWire === [1, 0, 0, 1]) {solve = P;}
-if (complexWire === [0, 0, 1, 0]) {solve = S;}
-if (complexWire === [0, 1, 1, 0]) {solve = C;}
-if (complexWire === [1, 1, 1, 0]) {solve = B;}
-if (complexWire === [0, 1, 0, 0]) {solve = C;}
-if (complexWire === [1, 1, 0, 0]) {solve = B;}
-if (complexWire === [1, 0, 0, 0]) {solve = D;}
-if (complexWire === [1, 0, 1, 0]) {solve = B;}
-if (complexWire === [1, 0, 1, 1]) {solve = P;}
+//find solve code
+
+//compare arrays Function
+console.log(complexWire.length);
+console.log(complexWire);
+
+const wireKey = {
+	C: [[0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 0, 0]],
+	D: [[1, 1, 1, 1], [0, 1, 0, 1], [1, 0, 0, 0]],
+  S: [[0, 0, 0, 1], [0, 0, 1, 1], [0, 0, 1, 0]],
+  P: [[0, 1, 1, 1], [1, 1, 0, 1], [1, 0, 0, 1], [1, 0, 1, 1]],
+  B: [[1, 1, 1, 0], [1, 1, 0, 0], [1, 0, 1, 0]],
+}
+
+//console.log(Object.keys(wireKey)); //keyset array
+
+//console.log('Match: ' + keyCompare(complexWire));
+
+const wireKeyset = Object.keys(wireKey); //keyset array
+
+function keyCompare(complexWire){
+
+  let keymatch = false; //default match to false
+  let currentLetter;
+
+    for (let i = 0; i < Object.keys(wireKey).length; i++) { //iterate letter
+
+          if (keymatch) break;
+          currentLetter = (Object.keys(wireKey)[i]);
+
+//        console.log(Object.keys(wireKey)[i]) //get letter in keyset array
+//        console.log('Current Letter: ' + currentLetter); //current letter
+//        console.log(wireKey[currentLetter]); //current letter's full array
+
+        for (let j = 0; j < wireKey[currentLetter].length; j++) { //iterate array in letter
+
+//          console.log(wireKey[currentLetter][j]); //current array
+
+          if (arrayCompare(complexWire, wireKey[currentLetter][j])) {
+            keymatch = true;
+            break;}
+
+        } //close for j
+
+    } //close for i
+
+//    console.log(currentLetter); //current letter
+
+    return currentLetter;
+
+  } //close function
+
+
+function arrayCompare(wireInput, wireManual) { //Compares input wire to manual
+
+  var match = true; //set default
+
+  if (wireInput.length !== wireManual.length) {
+    return 'No Way Jose';
+  }
+
+  for (i = 0; i < wireInput.length; i++) {
+
+    console.log(i+' loopvalue');
+
+    if (wireInput[i] !== wireManual[i]) {
+      match = false;
+      break;
+    }//close if
+
+  console.log(match+'end');
+  return match;
+	}//close for
+
+} //close function
+
+let solve = keyCompare(complexWire);
+
+console.log(solve);
 
 //solve --DONE
 function solveCode(solve) { // wrong function format?
-	if (solve === C) {return cut = 1;
+	if (solve === 'C') {return cut = 1;
 	}
-	if (solve === D) {return cut = 0;
+	if (solve === 'D') {return cut = 0;
 	}
-	if (solve === S && serialBool === 1) {return cut = 1;
+	if (solve === 'S' && serialBool === 1) {return cut = 1;
 	}
-	if (solve === P && parallelBool ==== 1) {return cut = 1;
+	if (solve === 'P' && parallelBool === 1) {return cut = 1;
 	}
-	if (solve === B && batteriesBool ==== 1) {return cut = 1;
+	if (solve === 'B' && batteriesBool === 1) {return cut = 1;
 	}
 	else return cut = 0; //not needed but catches the parameter = 0 cases
 }
 //print result cut yes/no
-if (solve.solveCode === 1 {console.log("CUT");
+console.log(solveCode(solve));
+if (solveCode(solve) === 1) {console.log("CUT");
 }
 else console.log("DON'T CUT");
 
